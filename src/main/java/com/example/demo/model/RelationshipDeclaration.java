b662252e90a17d90822351302109bcb1fb2db77d
bala-amypo <bala@amypo.in>
2025-12-21 14:07:14 +0000

    User commit

---
+++ src/main/java/com/example/demo/model/RelationshipDeclaration.java
@@ -0,0 +1,94 @@
+package com.example.demo.model;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
+
+@Entity
+public class RelationshipDeclaration {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long personId;
+
+    private String relatedPersonName;
+
+    private String relationshipType;   
+
+    private String description;
+
+    private LocalDateTime declaredAt;
+
+    private Boolean isVerified;
+
+    public RelationshipDeclaration() {
+    }
+
+    @PrePersist
+    public void onCreate() {
+        this.declaredAt = LocalDateTime.now();
+        if (this.isVerified == null) {
+            this.isVerified = false;
+        }
+    }
+
+ 
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getPersonId() {
+        return personId;
+    }
+
+    public void setPersonId(Long personId) {
+        this.personId = personId;
+    }
+
+    public String getRelatedPersonName() {
+        return relatedPersonName;
+    }
+
+    public void setRelatedPersonName(String relatedPersonName) {
+        this.relatedPersonName = relatedPersonName;
+    }
+
+    public String getRelationshipType() {
+        return relationshipType;
+    }
+
+    public void setRelationshipType(String relationshipType) {
+        this.relationshipType = relationshipType;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDateTime getDeclaredAt() {
+        return declaredAt;
+    }
+
+    public Boolean getIsVerified() {
+        return isVerified;
+    }
+
+    public void setIsVerified(Boolean isVerified) {
+        this.isVerified = isVerified;
+    }
+}
