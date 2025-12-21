7bd6fe8713a291df4ffabb3fb95a1d5f642cfa3f
bala-amypo <bala@amypo.in>
2025-12-21 14:10:14 +0000

    User commit

---
+++ src/main/java/com/example/demo/model/VendorEngagementRecord.java
@@ -0,0 +1,86 @@
+package com.example.demo.model;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class VendorEngagementRecord {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long employeeId;
+    private Long vendorId;
+
+    private String engagementType;
+    private Double amount;
+    private LocalDate engagementDate;
+    private String notes;
+
+   
+    public VendorEngagementRecord() {
+    }
+
+  
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(Long employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public Long getVendorId() {
+        return vendorId;
+    }
+
+    public void setVendorId(Long vendorId) {
+        this.vendorId = vendorId;
+    }
+
+    public String getEngagementType() {
+        return engagementType;
+    }
+
+    public void setEngagementType(String engagementType) {
+        this.engagementType = engagementType;
+    }
+
+    public Double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Double amount) {
+        this.amount = amount;
+    }
+
+    public LocalDate getEngagementDate() {
+        return engagementDate;
+    }
+
+    public void setEngagementDate(LocalDate engagementDate) {
+        this.engagementDate = engagementDate;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
+}
