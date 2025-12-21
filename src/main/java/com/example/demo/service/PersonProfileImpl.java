e89e9d1449280f21b8008972d4668a9e75c100da
bala-amypo <bala@amypo.in>
2025-12-21 13:59:10 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/PersonProfileImpl.java
@@ -0,0 +1,62 @@
+package com.example.demo.service.impl;
+
+import java.util.List;
+
+import com.example.demo.exception.ApiException;
+import com.example.demo.model.PersonProfile;
+import com.example.demo.repository.PersonProfileRepository;
+import com.example.demo.service.PersonProfileService;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class PersonProfileServiceImpl implements PersonProfileService {
+
+    private final PersonProfileRepository repository;
+
+    public PersonProfileServiceImpl(PersonProfileRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public PersonProfile createPerson(PersonProfile person) {
+
+        if (person.getEmail() == null || person.getReferenceId() == null) {
+            throw new ApiException("email or reference cannot be null");
+        }
+
+        if (repository.findByEmail(person.getEmail()).isPresent()) {
+            throw new ApiException("email already exists");
+        }
+
+        if (repository.findByReferenceId(person.getReferenceId()).isPresent()) {
+            throw new ApiException("reference already exists");
+        }
+
+        return repository.save(person);
+    }
+
+    @Override
+    public PersonProfile getPersonById(Long id) {
+        return repository.findById(id)
+                .orElseThrow(() -> new ApiException("person not found"));
+    }
+
+    @Override
+    public List<PersonProfile> getAllPersons() {
+        return repository.findAll();
+    }
+
+    @Override
+    public PersonProfile findByReferenceId(String referenceId) {
+        return repository.findByReferenceId(referenceId)
+                .orElseThrow(() -> new ApiException("person not found"));
+    }
+
+    @Override
+    public PersonProfile updateRelationshipDeclared(Long id, boolean declared) {
+        PersonProfile person = getPersonById(id);
+        person.setRelationshipDeclared(declared);
+        return repository.save(person);
+    }
+}
