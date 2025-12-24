4bd5e5fc81d268b95c830f44fd4debb711b6c5ff
bala-amypo <bala@amypo.in>
2025-12-24 14:00:33 +0000

    User commit

--- src/main/java/com/example/demo/controller/PersonProfileController.java
+++ src/main/java/com/example/demo/controller/PersonProfileController.java
@@ -1,4 +1,6 @@
 package com.example.demo.controller;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
 @RequestMapping("/api/persons")
--- src/main/java/com/example/demo/model/PersonProfile.java
+++ src/main/java/com/example/demo/model/PersonProfile.java
@@ -34,9 +34,7 @@ private String email;
         }
     }
     
-    public void setId(Long id) {
-    this.id = id;
-}
+
 
     public Long getId() {
         return id;
