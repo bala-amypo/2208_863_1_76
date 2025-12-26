7f14f2819b8d572d7da1086b4b7fb0a64b9b7672
bala-amypo <bala@amypo.in>
2025-12-26 07:58:06 +0000

    User commit

--- src/main/java/com/example/demo/config/OpenApiConfig.java
+++ src/main/java/com/example/demo/config/OpenApiConfig.java
@@ -1,9 +1,11 @@
 package com.example.demo.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
 import java.util.List;
 
 @Configuration
@@ -12,9 +14,14 @@ public class OpenApiConfig {
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
-              
+                .info(new Info()
+                        .title("Conflict of Interest Detection API")
+                        .description("Backend REST APIs")
+                        .version("1.0"))
                 .servers(List.of(
-                        new Server().url("https://9098.32procr.amypo.ai/login")
+                        new Server()
+                                .url("https://9098.32procr.amypo.ai")
+                                .descript       ion("Production Server")
                 ));
-        }
+    }
 }
